eeca4f4215c1949ccac08907600dbfe8d7af79bf
Alumno <Alumno@PC106>
2025-04-04 20:48:40 -0300

    Agregar archivos de proyecto.

---
+++ pryTisseraConexionBD/clsConexion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace pryTisseraConexionBD
+{
+    internal class clsConexion
+    {
+
+        string connectionString = "Server=E:/Escritorio/pryTisseraConexionBD;Database=Comercio;Trusted_Connection=True;";
+        public SqlConnection connection { get; set; }
+        public SqlConnection conexion()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                MessageBox.Show("✅ Conexión exitosa a la base de datos.");
+                return connection;
+ 
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return connection;
+            }
+
+        }
+    }
+}
